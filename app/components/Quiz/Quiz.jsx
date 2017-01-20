1402159e87b2adad7eb5849f979be0ae17cc073f
swanie21 <kswanie21@gmail.com>
2017-01-20 12:01:15 -0700

    refactor components and styling

--- app/components/Quiz/Quiz.jsx
+++ app/components/Quiz/Quiz.jsx
@@ -1,4 +1,5 @@
 import React, { PropTypes, Component } from 'react';
+import { IndexLink } from 'react-router';
 import Question from '../Question';
 
 if (process.env.BROWSER) {
@@ -10,6 +11,19 @@ class Quiz extends Component {
     super(props);
   }
 
+  resetScore() {
+    this.props.state.sumScore = 0;
+  }
+
+  answerFeedback(score) {
+    if(score === 10) {
+      alert('You are correct!');
+    }
+    if(score === 0) {
+      alert('Sorry, you are incorrect!');
+    }
+  }
+
   render() {
     const questions = this.props.state.getQuestions.questions[0];
     const { sumScore } = this.props.actions.quiz;
@@ -17,7 +31,7 @@ class Quiz extends Component {
 
     return (
       <section>
-        <h2 className='score'>Total Score: <span>{score}</span></h2>
+        <h1 className='quiz-title'>Colorado Questionnaire</h1>
         {questions.questions.map((question, index) =>
           <Question
             key={question.id}
@@ -25,8 +39,15 @@ class Quiz extends Component {
             question={question.question}
             answers={question.answers}
             sumScore={sumScore}
+            answerFeedback={this.answerFeedback.bind(this)}
           />
         )}
+        <h2 className='score'>Total Score: <span>{score}</span></h2>
+        <div className='button-container'>
+          <IndexLink to={'/'} className='nav-item-link' activeClassName='active' onClick={this.resetScore.bind(this)}>
+            <button className='retry-quiz-button'>Take Quiz Again</button>
+          </IndexLink>
+        </div>
       </section>
     );
   }
--- app/routes.js
+++ app/routes.js
@@ -2,14 +2,12 @@ import React from 'react';
 import { Route, IndexRoute } from 'react-router';
 import App from './components/App';
 import Main from './components/Main';
-import Counter from './components/Counter';
 import NoMatch from './components/NoMatch';
 import Quiz from './components/Quiz';
 
 export default (
   <Route path="/" component={App}>
     <IndexRoute component={Main} />
-    <Route path="/counter" component={Counter} />
     <Route path="/quiz" component={Quiz} />
     <Route path="/*" component={NoMatch} />
   </Route>
